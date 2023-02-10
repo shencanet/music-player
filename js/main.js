90189adbc88fc0f85d86a6066e95acd9e90a4dad
shen <santi.dionis@gmail.com>
2023-02-10 21:06:33 +0100

    first commit 2

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// Song data
+const songList = [
+    {
+        title: "Acoustic Breeze",
+        file: "acousticbreeze.mp3",
+        cover: "1.jpeg"
+
+    },
+    {
+        title: "A New Beginning",
+        file: "anewbeginning.mp3",
+        cover: "2.jpeg"
+    },
+    {
+        title: "Creative Minds",
+        file: "creativeminds.mp3",
+        cover: "3.jpeg"
+    },
+]
+
+// Capturar elementos del DOM para trabajar con JS
+const songs = document.getElementById("songs")
+const audio = document.getElementById("audio")
+const cover = document.getElementById("cover")
+const title = document.getElementById("title")
+const play = document.getElementById("play")
+const prev = document.getElementById("prev")
+const next = document.getElementById("next")
+const progress = document.getElementById("progress")
+const progressContainer = document.getElementById("progress-container")
